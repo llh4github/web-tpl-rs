725271b88d216cda02d2f8fcbc75bd44b53adeec
llh4github <lilinhong_coding@foxmail.com>
2025-03-02 07:42:16 +0800

    feat: add configuration management and application settings

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -1,32 +1,35 @@
-pub(crate) mod rsp;
 mod endpoint;
+pub(crate) mod rsp;
 
 use actix_web::middleware::Logger;
 use actix_web::{App, HttpServer};
+use common::cfg::{ApiNetwork, Settings};
 use log::error;
 use std::net::Ipv4Addr;
 use utoipa_actix_web::{scope, AppExt};
 use utoipa_swagger_ui::SwaggerUi;
 
 #[actix_web::main]
-async fn start() -> std::io::Result<()> {
+async fn start(network: ApiNetwork) -> std::io::Result<()> {
     let server = HttpServer::new(move || {
         let (app, api) = App::new()
             .into_utoipa_app()
             .map(|app| app.wrap(Logger::default()))
-            .service(scope::scope("/api").configure(|cfg| {
+            .service(scope::scope(network.prefix.as_str()).configure(|cfg| {
                 endpoint::apis(cfg);
             }))
             .split_for_parts();
         app.service(SwaggerUi::new("/swagger-ui/{_:.*}").url("/api-docs/openapi.json", api))
     })
-    .bind((Ipv4Addr::UNSPECIFIED, 8080))?;
+    .bind((Ipv4Addr::UNSPECIFIED, network.port))?;
     server.run().await?;
     Ok(())
 }
-pub fn main() {
-    flexi_logger::init();
-    let result = start();
+pub fn main(cfg: Settings) {
+    let fx_log = flexi_logger::Logger::try_with_env_or_str("debug")
+        .unwrap()
+        .start().expect("flexi_logger error");
+    let result = start(cfg.network);
 
     if let Some(err) = result.err() {
         error!("Error: {err}");
--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,4 +1,5 @@
-
+pub mod cfg;
+pub mod consts;
 
 pub const OK_STR: &str = "OK";
 pub const SUCCESS_STR: &str = "success";
