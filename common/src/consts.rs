d5d2b79131e4f66b0e017c25371821ba72480823
llh4github <lilinhong_coding@foxmail.com>
2025-03-02 15:03:54 +0800

    feat: update configuration file path and remove unused LogLevel enum

--- common/src/consts.rs
+++ common/src/consts.rs
@@ -1,17 +1,6 @@
-use serde::Serialize;
-
 /// The run mode of the application.
 pub type RunMode = &'static str;
 
 pub const DEV: RunMode = "development";
 pub const PROD: RunMode = "production";
 pub const TEST: RunMode = "test";
-#[repr(usize)]
-#[derive(Debug, Serialize)]
-pub enum LogLevel {
-    Error = 1,
-    Warn = 2,
-    Info = 3,
-    Debug = 4,
-    Trace = 5,
-}
