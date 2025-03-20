84d61364699629d9676f5eac413303be4aa4df17
llh4github <lilinhong_coding@foxmail.com>
2025-03-20 12:13:40 +0800

    feat: 更新角色 API 以使用 ReidsUtil 进行缓存操作并简化代码

--- api/src/endpoint/role.rs
+++ api/src/endpoint/role.rs
@@ -1,15 +1,10 @@
 use actix_web::{get, web};
-use cache::RedisConnectionManager;
-use common::cfg::AppCfg;
 use db::entities::{auth_role, prelude::AuthRole};
-use r2d2::Pool;
 use sea_orm::{DatabaseConnection, EntityTrait};
-use serde_json::json;
 use utoipa_actix_web::service_config::ServiceConfig;
-
 use crate::{
     rsp::{ApiResponse, ApiResult, ok_rsp},
-    util::CacheKeyUtil,
+    util::ReidsUtil,
 };
 
 pub(super) fn register_api(c: &mut ServiceConfig) {
@@ -31,33 +26,18 @@ const REDIS_KEY: &str = "role-module";
 #[get("/role/{id}")]
 async fn get_role(
     id: web::Path<i32>,
-    cfg: web::Data<AppCfg>,
-    redis_inject: web::Data<Pool<RedisConnectionManager>>,
-    cache_key_util: web::Data<CacheKeyUtil>,
+    redis_util: web::Data<ReidsUtil>,
     db_inject: web::Data<DatabaseConnection>,
 ) -> ApiResult<Option<auth_role::Model>> {
-    let key = cache_key_util.cache_key_i32(REDIS_KEY, *id);
-    let mut pool = redis_inject.get()?;
-    let cached: Option<String> = redis::cmd("GET").arg(&key).query(&mut pool)?;
-
+    let key = format!("{}:{}", REDIS_KEY, id);
+    let cached: Option<auth_role::Model> = redis_util.fetch_and_dejson(&key)?;
     if let Some(cached) = cached {
-        log::debug!("Cache found. role-id {}", *id);
-        let cached: Option<auth_role::Model> = serde_json::from_str(&cached).unwrap();
-        return ok_rsp(cached);
-    } else {
-        log::debug!("Cache not found, run db query. role-id {}", *id);
+        return ok_rsp(Some(cached));
     }
 
     let db = db_inject.get_ref();
 
     let option: Option<auth_role::Model> = AuthRole::find_by_id(*id).one(db).await?;
-    redis::cmd("SET")
-        .arg(&key)
-        .arg(json!(option).to_string())
-        .arg("EX")
-        .arg(cfg.cache.ttl)
-        .exec(&mut pool)?;
-
+    redis_util.cache_json_str(&key, &option)?;
     ok_rsp(option)
 }
-
